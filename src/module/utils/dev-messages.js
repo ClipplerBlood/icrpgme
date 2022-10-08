071a773a2e7e3d473675a062b2cee415de186550
ClipplerBlood <pqmanuelpq@gmail.com>
2022-10-08 11:28:23 +0200

    Started dev message implementation

--- src/module/utils/dev-messages.js
+++ src/module/utils/dev-messages.js
@@ -1,5 +1,19 @@
+import { i18n } from './utils.js';
+
 export function sendDevMessages() {
-  $.getJSON('https://raw.githubusercontent.com/pwatson100/symbaroum/master/msgdata/data.json', function (data) {
-    console.log(data);
+  $.getJSON('https://raw.githubusercontent.com/ClipplerBlood/icrpgme/dev-msg/src/dev-messages.json', function (data) {
+    if (data.messages === undefined || data.messages === null || data.messages.length === undefined) return;
+    data.messages.forEach(_sendMsg);
   });
+}
+
+function _sendMsg(msgData) {
+  const isGM = game.user.isGM;
+  if (msgData.gmOnly && !isGM) return;
+  ChatMessage.create({
+    speaker: ChatMessage.getSpeaker({ alias: i18n('FONTS.TypeSystem') }),
+    whisper: [game.user],
+    content: msgData.message,
+  });
+  console.log(msgData);
 }

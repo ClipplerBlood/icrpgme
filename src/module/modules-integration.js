92fdc37830897055f6911e57470848e4c0969778
ClipplerBlood <pqmanuelpq@gmail.com>
2022-10-10 11:27:11 +0200

    Improved dicesonice

--- src/module/modules-integration.js
+++ src/module/modules-integration.js
@@ -3,17 +3,20 @@ export function integrateExternalModules() {
 }
 
 function initDiceSoNice(dice3d) {
-  dice3d.addSystem({ id: 'icrpgme', name: 'Index Card RPG: Master Edition' }, true);
-  dice3d.addColorset({
-    name: 'Index Card RPG: Master Edition',
-    category: 'System',
-    description: 'ICRPG',
-    foreground: '#ffffff',
-    background: '#C02025FF',
-    outline: '#C02025FF',
-    edge: '#202020',
-    material: 'plastic',
-    font: 'Arial',
-    default: true,
-  });
+  dice3d.addSystem({ id: 'icrpgme', name: 'Index Card RPG: Master Edition' }, 'preferred');
+  dice3d.addColorset(
+    {
+      name: 'Index Card RPG: Master Edition',
+      category: 'System',
+      description: 'ICRPG',
+      foreground: '#ffffff',
+      background: '#C02025FF',
+      outline: '#C02025FF',
+      // edge: '#202020',
+      material: 'plastic',
+      font: 'Arial',
+      default: true,
+    },
+    'preferred',
+  );
 }
